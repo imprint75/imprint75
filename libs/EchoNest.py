493a224ddfb2718c7b0577d9ac0fbfc7eb6f40a8
sean <imprint75@gmail.com>
2016-02-18 14:09:24 +0200

    adding echonest funcs in libs

---
+++ libs/EchoNest.py
@@ -0,0 +1,43 @@
+import logging
+import requests
+
+from django.conf import settings
+from django.template.defaultfilters import slugify
+
+logger = logging.getLogger(__name__)
+
+
+def _make_call(options, *args, **kwargs):
+    params = '&'.join(['api_key={}'.format(settings.EN_API_KEY),
+                       'name={}'.format(options['value']),
+                       'format=json'])
+    r = requests.get('{}{}/{}?{}'.format(settings.ECHO_NEST_BASE_URL,
+                                         options['type'],
+                                         options['action'],
+                                         params))
+    res = r.json()
+    if res['response']['status']['message'] == "Success":
+        return res['response']
+    else:
+        return None
+
+
+def echo_lookup(name=None, type="artist"):
+    results = _make_call({"type": type,
+                          "action": "search",
+                          "value": name})
+    return results[''.join([type, 's'])]
+
+
+def echo_artist_info(id, method="images"):
+    results = _make_call({"type": "artist",
+                          "action": method,
+                          "value": id})
+    return results[method]
+
+
+def echo_get_artist(request):
+    try:
+        return slugify(request.POST['artist_search'])
+    except:
+        return slugify(settings.DEFAULT_ARTIST)
